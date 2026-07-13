2b4b90d4461130c4928e4e16a37d582a0c4b6392
lphyds <2774728374@qq.com>
2026-07-13 17:43:57 +0800

    Add files via upload
    通过辗转相除法快速得到两个正整数的最大公约数
    Obtain the greatest common divisor of two positive integers efficiently via the Euclidean algorithm.

---
+++ 辗转相除法.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	int t;
+	int ret = scanf("%d %d", &a, &b);
+
+	while (b != 0) {
+		t = a % b;
+		a = b;
+		b = t;
+	}
+	printf("gcd=%d\n", a);
+
+	return 0;
+}
